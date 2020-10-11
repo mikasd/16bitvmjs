0853ac3f78c81f93ceec216f1ceddea5fdb265e2
mikasd2 <test@pop-os.localdomain>
2020-10-11 00:04:13 -0700

    sets up module to create memory, creates CPU class, register setters/getters

---
+++ cpu.js
@@ -0,0 +1,38 @@
+const createMemory = require("./create-memory");
+
+class CPU {
+    constructor(memory) {
+        this.memory = memory;
+
+        this.registerNames = [
+            'ip', 'acc',
+            'r1', 'r2', 'r3', 'r4', 
+            'r5', 'r6', 'r7', 'r8',
+        ];
+
+        this.registers = createMemory(this.registerNames.length * 2);
+
+        this.registerMap = this.registeredNames.reduce((map, name, i) => {
+            map[name] = i * 2;
+            return map;
+        }, {});
+    }
+
+    getRegister(name) {
+        if (!(name in this.registerMap)) {
+            throw new Error(`getRegister: No such register '${name}'`);
+        }
+
+        return this.registers.getUint16(this.registerMap[name]);
+    }
+
+    setRegister(name, value) {
+        if (!(name in this.registerMap)) {
+            throw new Error(`setRegister: No such register '${name}'`);
+        }
+
+        return this.registers.getUint16(this.registerMap[name], value);
+    }
+}
+
+module.exports = CPU;
