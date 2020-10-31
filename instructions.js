fc5c5a7a14944dd9faf2e539bf2c1b1d9fa9f861
mike <michael.ingley@gmail.com>
2020-10-31 15:38:18 -0700

    adds new instructions

--- instructions.js
+++ instructions.js
@@ -1,29 +1,35 @@
-const MOV_LIT_REG = 0x10;
-const MOV_REG_REG = 0x11;
-const MOV_REG_MEM = 0x12;
-const MOV_MEM_REG = 0x13;
-const ADD_REG_REG = 0x14;
-const JMP_NOT_EQ  = 0x15;
-const PSH_LIT     = 0x17;
-const PSH_REG     = 0x18;
-const POP         = 0x1A;
-const CAL_LIT     = 0x5E;
-const CAL_REG     = 0x5F;
-const RET         = 0x60;
-const HLT         = 0xFF;
+const MOV_LIT_REG       = 0x10;
+const MOV_REG_REG       = 0x11;
+const MOV_REG_MEM       = 0x12;
+const MOV_MEM_REG       = 0x13;
+const MOV_LIT_MEM       = 0x1b;
+const MOV_REG_PTR_REG   = 0x1c;
+const MOV_LIT_OFF_REG   = 0x1d;
+const ADD_REG_REG       = 0x14;
+const JMP_NOT_EQ        = 0x15;
+const PSH_LIT           = 0x17;
+const PSH_REG           = 0x18;
+const POP               = 0x1a;
+const CAL_LIT           = 0x5e;
+const CAL_REG           = 0x5f;
+const RET               = 0x60;
+const HLT               = 0xff;
 
 module.exports = {
-    MOV_LIT_REG,
-    MOV_REG_REG,
-    MOV_REG_MEM,
-    MOV_MEM_REG,
-    ADD_REG_REG,
-    JMP_NOT_EQ,
-    PSH_LIT,
-    PSH_REG,
-    POP,
-    CAL_LIT,
-    CAL_REG,
-    RET,
-    HLT,    
-};  
+  MOV_LIT_REG,
+  MOV_REG_REG,
+  MOV_REG_MEM,
+  MOV_MEM_REG,
+  MOV_LIT_MEM,
+  MOV_REG_PTR_REG,
+  MOV_LIT_OFF_REG,
+  ADD_REG_REG,
+  JMP_NOT_EQ,
+  PSH_LIT,
+  PSH_REG,
+  POP,
+  CAL_LIT,
+  CAL_REG,
+  RET,
+  HLT,
+};
